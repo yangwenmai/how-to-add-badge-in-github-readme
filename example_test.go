385e124137d8fd49f86f758d68d7fd18389b3ce6
yangwenmai <yangwen.yw@gmail.com>
2017-12-06 18:53:23 +0800

    restore test cover

--- example_test.go
+++ example_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestShowProjectName(t *testing.T) {
 	Convey("test add", t, func() {
-		//a := 3
-		//b := 1
-		//c := Add(a, b)
-		//So(c, ShouldEqual, 4)
+		a := 3
+		b := 1
+		c := Add(a, b)
+		So(c, ShouldEqual, 4)
 	})
 }
